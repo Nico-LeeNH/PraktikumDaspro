136acc521e4bdcde2a8daeed282ebdfa2f5de61d
Nico Lee <new.nikolee2007@gmail.com>
2025-10-02 09:00:01 +0700

    Modifikasi percobaan 2

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -5,12 +5,10 @@ public class ifCetakKRS20 {
         Scanner sc = new Scanner(System.in);
         System.out.print("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
-        boolean uktLunas = input.nextBoolean();
+        boolean uktLunas = sc.nextBoolean();
         if (uktLunas) {
             System.out.print("Pembayaran UKT terverifikasi");
             System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
+        } 
         }
     }
