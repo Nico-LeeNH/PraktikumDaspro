d63266e144adc7a908747202f6535c2b0ba3839b
Nico Lee <new.nikolee2007@gmail.com>
2025-10-02 09:18:06 +0700

    Modifikasi Percobaan 2

---
+++ jobsheet5/ifElseCetakKRS20.java
@@ -0,0 +1,29 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifElseCetakKRS20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("--- Cetak KRS SIAKAD ---");
+        System.out.print("Masukkan semester saat ini: ");
+        int semester = sc.nextInt();
+
+        if (semester == 1) {
+            System.out.print("KRS Semester 1 ditampilkan");
+        } else if (semester == 2) {
+            System.out.print("KRS Semester 2 ditampilkan");
+        } else if (semester == 3) {
+            System.out.print("KRS Semester 3 ditampilkan");
+        } else if (semester == 4) {
+            System.out.print("KRS Semester 4 ditampilkan");
+        } else if (semester == 5) {
+            System.out.print("KRS Semester 5 ditampilkan");
+        } else if (semester == 6) {
+            System.out.print("KRS Semester 6 ditampilkan");
+        } else if (semester == 7) {
+            System.out.print("KRS Semester 7 ditampilkan");
+        } else if (semester == 8) {
+            System.out.print("KRS Semester 8 ditampilkan");
+        } else {
+            System.out.println("Semester tidak valid");
+        }
+    }
