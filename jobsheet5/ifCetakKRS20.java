4d07c82c7dc9a8181b1aff27ec4be876f708f8eb
Nico Lee <new.nikolee2007@gmail.com>
2025-10-02 10:32:38 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -6,6 +6,10 @@ public class ifCetakKRS20 {
         System.out.print("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
+        // ternary operator
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Pembayaran UKT belum lunas\nSilahkan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+
         if (uktLunas) {
             System.out.print("Pembayaran UKT terverifikasi");
             System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
